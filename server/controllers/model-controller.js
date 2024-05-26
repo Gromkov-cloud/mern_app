9f83a18afed5d8d2dd383d3b25efd409a924f950
macbook <macbook@MacBook-Pro-macbook.local>
2024-05-26 17:04:22 +0500

    удалил лишнее, перенес тяжелые файлы

--- server/controllers/model-controller.js
+++ server/controllers/model-controller.js
@@ -1,218 +1,219 @@
-const S3Service = require("../services/s3-service")
+const S3Service = require("../services/s3-service");
 const Model = require("../models/model-model");
 const s3Service = require("../services/s3-service");
-var ObjectId = require('mongodb').ObjectId;
-require('dotenv').config()
-
+var ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
 
 const pickFiles = (files, reqBody) => {
-    // возвращает тело запроса с свойством s3 где записываются файлы,
-    // которые были изменены(которые пришли с клиента)
-    // в случае если файлы не были изменены, то есть req.files пустой
-    // то тело запроса не будет изменено
+	// возвращает тело запроса с свойством s3 где записываются файлы,
+	// которые были изменены(которые пришли с клиента)
+	// в случае если файлы не были изменены, то есть req.files пустой
+	// то тело запроса не будет изменено
 
-    if (files.length) {
-        const s3Files = {}
+	if (files.length) {
+		const s3Files = {};
 
-        files.forEach(file => {
-            if (file.metadata.fileType == "application/octet-stream") {
-                const model = {
-                    "fileName": file.originalname,
-                    "size": file.size,
-                    "s3Name": file.key
-                }
-                s3Files.model = { ...model }
-            } else {
-                const qr = {
-                    "fileName": file.originalname,
-                    "s3Name": file.key
-                }
-                s3Files.qr = { ...qr }
-            }
-        });
-        reqBody.s3 = { ...s3Files }
-        return reqBody
-    } else {
-        return reqBody
-    }
-
-}
+		files.forEach((file) => {
+			if (file.metadata.fileType == "application/octet-stream") {
+				const model = {
+					fileName: file.originalname,
+					size: file.size,
+					s3Name: file.key,
+				};
+				s3Files.model = { ...model };
+			} else {
+				const qr = {
+					fileName: file.originalname,
+					s3Name: file.key,
+				};
+				s3Files.qr = { ...qr };
+			}
+		});
+		reqBody.s3 = { ...s3Files };
+		return reqBody;
+	} else {
+		return reqBody;
+	}
+};
 const getDeleteParams = (modelFileNames) => {
-    modelFileNames = { ...modelFileNames }
-    const params = {
-        Bucket: process.env.S3_BUCKET_NAME,
-        Delete: {
-            Objects: [],
-        },
-    }
+	modelFileNames = { ...modelFileNames };
+	const params = {
+		Bucket: process.env.S3_BUCKET_NAME,
+		Delete: {
+			Objects: [],
+		},
+	};
 
-    for (const key in modelFileNames) {
-        if (modelFileNames[key]) {
-            params.Delete.Objects.push({ Key: modelFileNames[key] })
-        }
-    }
+	for (const key in modelFileNames) {
+		if (modelFileNames[key]) {
+			params.Delete.Objects.push({ Key: modelFileNames[key] });
+		}
+	}
 
-    return params
-}
+	return params;
+};
 
 const getModelDTO = (reqBody, oldData) => {
-    const modelDTO = JSON.parse(JSON.stringify(oldData))
+	const modelDTO = JSON.parse(JSON.stringify(oldData));
 
-    if (reqBody.modelName) { modelDTO.name = reqBody.modelName }
-    if (reqBody.description) { modelDTO.description = reqBody.description }
-    if (reqBody.isActive) { modelDTO.isActive = reqBody.isActive }
-    if (reqBody.s3) {
-        if (reqBody.s3.model) {
-            modelDTO.fileName = reqBody.s3.model.fileName
-            modelDTO.size = reqBody.s3.model.size
-            modelDTO.s3.model = reqBody.s3.model.s3Name
-        }
-        if (reqBody.s3.image) {
-            modelDTO.imageName = reqBody.s3.image.fileName
-            modelDTO.s3.image = reqBody.s3.image.s3Name
-        }
-    }
+	if (reqBody.modelName) {
+		modelDTO.name = reqBody.modelName;
+	}
+	if (reqBody.description) {
+		modelDTO.description = reqBody.description;
+	}
+	if (reqBody.isActive) {
+		modelDTO.isActive = reqBody.isActive;
+	}
+	if (reqBody.s3) {
+		if (reqBody.s3.model) {
+			modelDTO.fileName = reqBody.s3.model.fileName;
+			modelDTO.size = reqBody.s3.model.size;
+			modelDTO.s3.model = reqBody.s3.model.s3Name;
+		}
+		if (reqBody.s3.image) {
+			modelDTO.imageName = reqBody.s3.image.fileName;
+			modelDTO.s3.image = reqBody.s3.image.s3Name;
+		}
+	}
 
-    return modelDTO
-}
+	return modelDTO;
+};
 
 const getDeleteOptions = (data, oldData) => {
-    const objects = []
+	const objects = [];
 
-    if (data.s3.model) {
-        objects.push({ "Key": oldData.s3.model })
-    }
-    if (data.s3.image) {
-        objects.push({ "Key": oldData.s3.image })
-    }
+	if (data.s3.model) {
+		objects.push({ Key: oldData.s3.model });
+	}
+	if (data.s3.image) {
+		objects.push({ Key: oldData.s3.image });
+	}
 
-    return {
-        Bucket: process.env.S3_BUCKET_NAME,
-        Delete: { "Objects": objects, Quiet: false }
-    }
-}
-
+	return {
+		Bucket: process.env.S3_BUCKET_NAME,
+		Delete: { Objects: objects, Quiet: false },
+	};
+};
 
 class ModelController {
+	async getModel(req, res) {
+		const model = await Model.findById(req.params.id);
+		console.log(model);
 
-    async getModel(req, res) {
+		const params = {
+			Bucket: process.env.S3_BUCKET_NAME,
+			Key: model.s3.model,
+		};
 
-        const model = await Model.findById(req.params.id)
-        console.log(model)
+		const readStream = S3Service.s3.getObject(params).createReadStream();
 
-        const params = {
-            Bucket: process.env.S3_BUCKET_NAME,
-            Key: model.s3.model
-        };
+		readStream.on("data", (data) => {
+			res.write(data);
+		});
 
-        const readStream = S3Service.s3.getObject(params).createReadStream();
+		readStream.on("end", () => {
+			res.end();
+		});
+	}
+	async getModels(req, res) {
+		const filters = req.query;
+		const models = await Model.find(filters);
+		res.status(200).json(models);
+	}
+	async postModel(req, res) {
+		console.log("КОНТРОЛЛЕР ДОВАЛЕНИЯ МОДЕЛИ");
+		const modelDto = {
+			name: req.body.modelName,
+			fileName: req.files[0].originalname,
+			imageName: req.files[1]?.originalname || null,
+			date: new Date(),
+			description: req.body.description || null,
+			size: req.files[0].size,
+			isActive: true,
+			s3: {
+				model: req.files[0].key,
+				image: null,
+				qr: req.files[1].key,
+			},
+		};
+		const model = new Model(modelDto);
+		const result = await model.save();
+		res.status(200).json({ result: result });
+	}
+	// async updateModel(req, res) {
 
-        readStream.on('data', (data) => {
-            res.write(data);
-        });
+	//     const oldModel = await Model.findById(req.body.id)
+	//     const data = pickFiles(req.files, req.body)
+	//     const modelDto = getModelDTO(data, oldModel)
+	//     modelDto.updateDate = new Date()
 
-        readStream.on('end', () => {
-            res.end();
-        });
-    }
-    async getModels(req, res) {
-        const filters = req.query
-        const models = await Model.find(filters)
-        res.status(200).json(models)
-    }
-    async postModel(req, res) {
-        const modelDto = {
-            name: req.body.modelName,
-            fileName: req.files[0].originalname,
-            imageName: req.files[1]?.originalname || null,
-            date: new Date(),
-            description: req.body.description || null,
-            size: req.files[0].size,
-            isActive: true,
-            s3: {
-                model: req.files[0].key,
-                image: null,
-                qr: req.files[1].key
-            }
-        }
-        const model = new Model(modelDto)
-        const result = await model.save()
-        res.status(200).json({ "result": result })
-    }
-    // async updateModel(req, res) {
+	//     const result = await Model.updateOne({ "_id": new ObjectId(req.body.id) }, modelDto)
 
-    //     const oldModel = await Model.findById(req.body.id)
-    //     const data = pickFiles(req.files, req.body)
-    //     const modelDto = getModelDTO(data, oldModel)
-    //     modelDto.updateDate = new Date()
+	//     if (data.s3) {
+	//         s3Service.s3.deleteObjects(getDeleteOptions(data, oldModel), function (err, data) {
+	//             if (data) {
+	//                 console.log("File successfully deleted", data);
+	//             } else {
+	//                 console.log("Check with error message " + err);
+	//             }
+	//         });
+	//     }
 
-    //     const result = await Model.updateOne({ "_id": new ObjectId(req.body.id) }, modelDto)
+	//     res.json(result)
+	// }
 
-    //     if (data.s3) {
-    //         s3Service.s3.deleteObjects(getDeleteOptions(data, oldModel), function (err, data) {
-    //             if (data) {
-    //                 console.log("File successfully deleted", data);
-    //             } else {
-    //                 console.log("Check with error message " + err);
-    //             }
-    //         });
-    //     }
+	async updateModel(req, res) {
+		res.json({ missage: "sosa" });
+	}
 
-    //     res.json(result)
-    // }
+	async deleteModel(req, res) {
+		const id = req.params.id;
 
-    async updateModel(req, res) {
-        res.json({ "missage": "sosa" })
-    }
+		const model = await Model.findById(id);
+		if (!model) {
+			res.json("такой модели не найдено");
+		}
 
-    async deleteModel(req, res) {
-        const id = req.params.id
+		const deletedModel = await Model.deleteOne({ _id: id });
 
-        const model = await Model.findById(id)
-        if (!model) {
-            res.json("такой модели не найдено")
-        }
+		S3Service.s3.deleteObjects(getDeleteParams(model.s3), (err, data) => {
+			if (err) {
+				console.log(err, err.stack);
+				res.status(500).json("Произошла ошибка при удалении файлов");
+			} else {
+				res.json("Файлы успешно удалены!");
+			}
+		});
+		res.status(200);
+	}
 
-        const deletedModel = await Model.deleteOne({ _id: id })
+	async getModelInfo(req, res) {
+		const id = req.params.id;
+		const info = await Model.findById(id);
+		console.log(info);
+		res.status(200).json(info);
+	}
 
-        S3Service.s3.deleteObjects(getDeleteParams(model.s3), (err, data) => {
-            if (err) {
-                console.log(err, err.stack);
-                res.status(500).json('Произошла ошибка при удалении файлов');
-            } else {
-                res.json('Файлы успешно удалены!');
-            }
-        });
-        res.status(200)
-    }
+	async getModelQr(req, res) {
+		const model = await Model.findById(req.params.id);
+		console.log(model);
 
-    async getModelInfo(req, res) {
-        const id = req.params.id
-        const info = await Model.findById(id)
-        console.log(info)
-        res.status(200).json(info)
-    }
+		const params = {
+			Bucket: process.env.S3_BUCKET_NAME,
+			Key: model.s3.qr,
+		};
 
-    async getModelQr(req, res) {
-        const model = await Model.findById(req.params.id)
-        console.log(model)
+		const readStream = S3Service.s3.getObject(params).createReadStream();
 
-        const params = {
-            Bucket: process.env.S3_BUCKET_NAME,
-            Key: model.s3.qr
-        };
+		readStream.on("data", (data) => {
+			res.write(data);
+		});
 
-
-        const readStream = S3Service.s3.getObject(params).createReadStream();
-
-        readStream.on('data', (data) => {
-            res.write(data);
-        });
-
-        readStream.on('end', () => {
-            res.end();
-        });
-    }
+		readStream.on("end", () => {
+			res.end();
+		});
+	}
 }
 
-module.exports = new ModelController()
+module.exports = new ModelController();
--- server/routes/model-router.js
+++ server/routes/model-router.js
@@ -1,16 +1,39 @@
-const Router = require("express")
-const router = new Router()
+const Router = require("express");
+const router = new Router();
 
-const modelController = require("../controllers/model-controller")
-const multerService = require("../services/multer-service")
-const authMiddleware = require("../middleware/auth-middleware")
+const modelController = require("../controllers/model-controller");
+const multerService = require("../services/multer-service");
+const authMiddleware = require("../middleware/auth-middleware");
 
-router.get("/model/:id", modelController.getModel) // получение модели (3d) (ya bucket)
-router.get("/models", modelController.getModels) // получение всех моделей (текстовой информации)
-router.get("/model-qr/:id", modelController.getModelQr) // получить картинку qr кода
-router.get("/model-info/:id", modelController.getModelInfo) // получене одной модели (текстовой информации)
-router.delete("/model/:id", authMiddleware, modelController.deleteModel) // удалить модель
-router.post("/model/add", authMiddleware, multerService.upload.array("files", 2), modelController.postModel) // добавить модель
-router.post("/model/update/:id", authMiddleware, multerService.upload.array("files", 2), modelController.updateModel) // обновить модель
+// получение модели (3d) (ya bucket)
+router.get("/model/:id", modelController.getModel);
 
-module.exports = router
+// получение всех моделей (текстовой информации)
+router.get("/models", modelController.getModels);
+
+// получить картинку qr кода
+router.get("/model-qr/:id", modelController.getModelQr);
+
+// получене одной модели (текстовой информации)
+router.get("/model-info/:id", modelController.getModelInfo);
+
+// удалить модель
+router.delete("/model/:id", authMiddleware, modelController.deleteModel);
+
+// добавить модель
+router.post(
+  "/model/add",
+  authMiddleware,
+  multerService.upload.array("files", 2),
+  modelController.postModel
+);
+
+// обновить модель
+router.post(
+  "/model/update/:id",
+  authMiddleware,
+  multerService.upload.array("files", 2),
+  modelController.updateModel
+);
+
+module.exports = router;
